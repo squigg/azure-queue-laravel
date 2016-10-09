1faa00a82df67a38219b16657f3f282fe1a14699
Squigg <github@squigg.co.uk>
2016-10-09 15:07:37 +0100

    Remove zero entry on array_fill for PHP 5.5
    (cherry picked from commit 04887aa)

--- tests/Fixtures/ListMessagesResult.php
+++ tests/Fixtures/ListMessagesResult.php
@@ -17,15 +17,16 @@ class ListMessagesResult
 
     /**
      * ListMessagesResult constructor.
+     * @param int $count
      */
     public function __construct($count = 1)
     {
-
         $this->count = $count;
     }
 
     public function getQueueMessages()
     {
+        if ($this->count == 0) return [];
         return array_fill(0, $this->count, new MicrosoftAzureQueueMessage());
     }
 }
