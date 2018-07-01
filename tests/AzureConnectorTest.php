aacad6f0bebd72dfbce34d9be4ac8e9d40bcd8a2
analogwerk <info@analogwerk.de>
2018-07-01 19:40:57 +0200

    Add EndpointSuffix configuration Closes #7.

--- tests/AzureConnectorTest.php
+++ tests/AzureConnectorTest.php
@@ -17,10 +17,19 @@ class AzureConnectorTest extends TestCase
      * @var \Mockery\Mock
      */
     protected $queueRestProxy;
+    protected $config;
 
     public function setUp()
     {
         parent::setUp();
+
+        $this->config = [
+            'protocol' => 'https',
+            'accountname' => 'foo',
+            'key' => 'bar',
+            'queue' => 'baz',
+            'timeout' => 25,
+        ];
 
         $this->connector = new AzureConnector();
         $this->queueRestProxy = Mockery::mock('alias:' . QueueRestProxy::class);
@@ -29,23 +38,28 @@ class AzureConnectorTest extends TestCase
     /** @test */
     public function it_can_create_azure_queue()
     {
-        $config = [
-            'protocol' => 'https',
-            'accountname' => 'foo',
-            'key' => 'bar',
-            'queue' => 'baz',
-            'timeout' => 25,
-        ];
-
-        $connectionString = 'DefaultEndpointsProtocol=' . $config['protocol'] . ';AccountName=' . $config['accountname'] . ';AccountKey=' . $config['key'];
+        $connectionString = 'DefaultEndpointsProtocol=https;AccountName=foo;AccountKey=bar';
         $queueProxy = Mockery::mock(IQueue::class);
 
         $this->queueRestProxy->shouldReceive('createQueueService')->once()->with($connectionString)->andReturn($queueProxy);
 
         /** @var AzureQueue $azureQueue */
-        $azureQueue = $this->connector->connect($config);
+        $azureQueue = $this->connector->connect($this->config);
         $this->assertEquals('baz', $azureQueue->getQueue(null));
         $this->assertEquals(25, $azureQueue->getVisibilityTimeout());
+    }
+
+    /** @test */
+    public function it_can_create_azure_queue_with_endpoint()
+    {
+        $this->config['endpoint'] = 'mysuffix';
+
+        $connectionString = 'DefaultEndpointsProtocol=https;AccountName=foo;AccountKey=bar;EndpointSuffix=mysuffix';
+        $queueProxy = Mockery::mock(IQueue::class);
+        $this->queueRestProxy->shouldReceive('createQueueService')->once()->with($connectionString)->andReturn($queueProxy);
+
+        /** @var AzureQueue $azureQueue */
+        $this->connector->connect($this->config);
     }
 
 }
