068503259cf0bf7fbd3a77f27378aa9afb2d90dc
Rik Morskate <rik@orderchamp.com>
2022-07-17 21:56:49 +0200

    Correct description

--- src/AzureQueue.php
+++ src/AzureQueue.php
@@ -128,7 +128,6 @@ class AzureQueue extends Queue implements QueueInterface, ClearableQueue
      * Delete all of the jobs from the queue.
      *
      * @param  string  $queue
-     * @return int
      */
     public function clear($queue)
     {
